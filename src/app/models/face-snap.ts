9bbd0adb20b7aeb7e2acc44b9f7a04365a70fc42
Guyon Clément <g.clem2016@gmail.com>
2024-11-28 11:02:06 +0100

    Ajout et affichage du parametre de localisation

--- src/app/models/face-snap.ts
+++ src/app/models/face-snap.ts
@@ -1,12 +1,20 @@
 export class FaceSnap {
-  constructor(public title: string, public description: string, public createdDate: Date, public snaps: number, public imageUrl: string) {
-  }
 
-    addSnap() {
+    location ?: string;
+
+    constructor(public title: string, public description: string, public createdDate: Date, public snaps: number, public imageUrl: string) {
+    }
+
+    addSnap(): void {
         this.snaps++;
     }
 
-    removeSnap() {
+    removeSnap(): void {
         this.snaps--;
     }
+
+    setLocation(location: string): void {
+        this.location = location;
+    }
+
 }
