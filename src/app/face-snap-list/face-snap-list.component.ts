9091561393ce05c9238adde2e0950fe8218a0645
Guyon Clément <g.clem2016@gmail.com>
2024-12-03 11:34:52 +0100

    implémentation de l'utilisation du service

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -11,12 +12,9 @@ import { FaceSnapComponent } from '../face-snap/face-snap.component';
 export class FaceSnapListComponent implements OnInit{
   faceSnaps!: FaceSnap[];
 
+  constructor(private faceSnapsService: FaceSnapsService) {}
+
   ngOnInit(): void {
-    this.faceSnaps = [
-    new FaceSnap('Face Snap', 'This is a face snap component', new Date(), 0, 'https://www.w3schools.com/howto/img_avatar.png')
-    ,new FaceSnap('Second Face Snap', 'This is my second face snap component', new Date(), 0, 'https://images.unsplash.com/photo-1724685010983-2770a37658b0?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
-    ,new FaceSnap('Last Face Snap', 'This is my last face snap component', new Date(), 200, 'https://images.unsplash.com/photo-1723039914210-a6f13a6931af?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
-    ];
-    this.faceSnaps[1].setLocation('New York');
+    this.faceSnaps = this.faceSnapsService.getFaceSnaps();
   }
 }
--- src/app/models/face-snap.ts
+++ src/app/models/face-snap.ts
@@ -17,4 +17,8 @@ export class FaceSnap {
         this.location = location;
     }
 
+    withLocation(location: string): FaceSnap {
+        this.setLocation(location);
+        return this;
+    }
 }
