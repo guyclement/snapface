a5896381146280106868f1920a5b9657bb4bf082
Guyon Clément <g.clem2016@gmail.com>
2024-12-03 11:24:10 +0100

    Ajout du header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnap } from './models/face-snap';
 import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FaceSnapListComponent],
+  imports: [FaceSnapListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
