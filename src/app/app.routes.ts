564117c97552ef956c22f478d7bd9dc9300e8ebe
Guyon Clément <g.clem2016@gmail.com>
2024-12-04 10:51:09 +0100

    création d'une landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { LandingPageComponentComponent } from './landing-page-component/landing-page-component.component';
 
 export const routes: Routes = [
     { path: 'facesnaps', component: FaceSnapListComponent },
+    { path: '', component: LandingPageComponentComponent },
 ];
