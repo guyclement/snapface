2b460b1573694e918c50f044aa35820fdae50ed3
Guyon Clément <g.clem2016@gmail.com>
2024-12-04 10:27:40 +0100

    Ajout de la route facesnaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
-import { FaceSnap } from './models/face-snap';
-import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FaceSnapListComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
